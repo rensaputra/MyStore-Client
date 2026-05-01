88c0429b7f054484f28d1a1878e7411a747a95d4
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-05-01 10:11:45 +1000

    feat: fix build issue by wrapping Suspense boundary on Header component

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Header from "@/components/layout/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
+      <Suspense fallback={<div className="h-16" />}>
+        <Header />
+      </Suspense>
       <div className="container">{children}</div>
     </>
   );
