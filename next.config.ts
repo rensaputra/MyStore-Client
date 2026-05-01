ecfdfcf5d2ddcffa208e4631d20c0140f49b8b1a
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-05-01 12:54:58 +1000

    feat: migrate to vercel blob storage

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,9 @@ const nextConfig: NextConfig = {
     unoptimized: process.env.NODE_ENV === "development",
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/uploads/**",
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+        pathname: "/**",
       },
     ],
   },
